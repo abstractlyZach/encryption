29381e68f00817dbb9844ef28e432c089bf94e03
Zach Lee <zach.lee@concur.com>
2018-01-31 16:12:08 -0800

    Add text reading utility

--- encryption/utils.py
+++ encryption/utils.py
@@ -4,3 +4,7 @@ def get_english_dictionary():
     with open('data/words_dictionary.json', 'r') as infile:
         english_dictionary = json.loads(infile.read())
         return english_dictionary
+
+def get_text_as_string(file_name):
+    with open('data/{}.txt'.format(file_name), 'r') as infile:
+        return infile.read()
